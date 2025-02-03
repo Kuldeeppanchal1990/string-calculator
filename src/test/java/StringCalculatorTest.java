a081d435e9ed07f1dee1757c8c8ea00ed3e8fda6
Kuldeep Panchal <kuldeepsubscription@gmail.com>
2025-02-03 23:37:08 +0530

    Test case - return sum when nos passed with comma or slash N separated

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,6 +2,8 @@ import org.incubyte.StringCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,5 +31,15 @@ public class StringCalculatorTest {
         assertEquals(3,stringCalculator.calculate("1,2"));
     }
 
+    @Test
+    public void returnSum_WhenMoreThanTwoNosArePassed() {
+        assertEquals(6, stringCalculator.calculate("1,2,3"));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3:6","'1\n2\n3':6"}, delimiter = ':')
+    public void returnSum_WhenCommaOrSlashNUsedAsDelimiter(String input, int expectedResult) {
+        assertEquals(expectedResult, stringCalculator.calculate(input));
+    }
 
 }
--- src/test/java/org/incubyte/StringCalculator.java
+++ src/test/java/org/incubyte/StringCalculator.java
@@ -4,10 +4,11 @@ public class StringCalculator {
 
 
     public static final String COMMA_SEPARATED = ",";
+    public static final String SLASHN_SEPARATED = "\n";
 
     public int calculate(String number) {
 
-        String[] nos = number.split(COMMA_SEPARATED);
+        String[] nos = number.split(COMMA_SEPARATED + "|" + SLASHN_SEPARATED);
 
         int sum = 0;
         for(String no : nos) {
