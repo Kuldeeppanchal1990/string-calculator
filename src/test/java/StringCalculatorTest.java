abfb5b98d08bc59f76e277f6984b9c2f0c1164b2
Kuldeep Panchal <kuldeepsubscription@gmail.com>
2025-02-03 23:05:57 +0530

    Refactoring

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -2,12 +2,14 @@ import org.incubyte.StringCalculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
 
     @Test
     public void returnZero_WhenZeroIsPassed() {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.calculate("0");
-        Assertions.assertEquals(0,result);
+        assertEquals(0,result);
     }
 }
